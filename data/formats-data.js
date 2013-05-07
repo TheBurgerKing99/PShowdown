5925f23fc67e8d5f3156412f23bdee592e4ebb39
Nollan101 <kgb@kgbudge.com>
2013-05-06 21:39:08 -0600

    Add Electaroo

--- data/formats-data.js
+++ data/formats-data.js
@@ -15,6 +15,12 @@ exports.BattleFormatsData = {
 		dreamWorldRelease: true,
 		tier: "USER"
 	},
+	electaroo: {
+                viableMoves: {"wildcharge":1,"hijumpkick":1,"superpower":1,"thunderpunch":1,"stoneedge":1,"acrobatics":1,"bulkup":1,"substitute":1,"focuspunch":1,"machpunch":1,"drainpunch":1},
+                isNonstandard: true,
+                dreamWorldRelease: true,
+                tier: "G5CAP"
+        },
 	tomohawk: {
 		viableMoves: {"aurasphere":1,"roost":1,"stealthrock":1,"rapidspin":1,"hurricane":1,"airslash":1,"taunt":1,"substitute":1,"toxic":1},
 		isNonstandard: true,
