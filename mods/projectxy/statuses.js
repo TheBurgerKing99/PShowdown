380b91418e224879d4e5189cdf61dad247d2e1da
Nollan101 <kgb@kgbudge.com>
2013-05-17 17:05:24 -0500

    Add Freeze and Hail changes

--- mods/projectxy/statuses.js
+++ mods/projectxy/statuses.js
@@ -5,4 +5,32 @@ function clampIntRange(num, min, max) {
 	return num;
 }
 exports.BattleStatuses = {
+	frz: {
+		effectType: 'Status',
+		onStart: function(target) {
+			this.add('-status', target, 'frz');
+		},
+		onBeforeMovePriority: 2,
+		onBeforeMove: function(pokemon, target, move) {
+			if (move.thawsUser || this.random(2) === 0) {
+				pokemon.cureStatus();
+				return;
+			}
+			this.add('cant', pokemon, 'frz');
+			return false;
+		},
+		onHit: function(target, source, move) {
+			if (move.type === 'Fire' && move.category !== 'Status') {
+				target.cureStatus();
+			}
+		}
+	},
+	hail: {
+		inherit: true,
+		onModifyDef: function(def, pokemon) {
+			if (pokemon.hasType('Ice') && this.isWeather('hail')) {
+				return def * 3/2;
+			}
+		}
+	}
 };
