c5e09ab5162e44dfa05f987e14c575ccf27b6958
Nollan101 <kgb@kgbudge.com>
2013-05-17 16:58:05 -0500

    Create statuses.js

---
+++ mods/projectxy/statuses.js
@@ -0,0 +1,8 @@
+function clampIntRange(num, min, max) {
+  num = Math.floor(num);
+	if (num < min) num = min;
+	if (typeof max !== 'undefined' && num > max) num = max;
+	return num;
+}
+exports.BattleStatuses = {
+};
