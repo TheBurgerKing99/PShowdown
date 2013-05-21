b7f4e77af5caecdf83875f77c3434136e46f6939
TheBurgerKing99 <theburgerking96@gmail.com>
2013-05-20 21:40:57 -0300

    Update pokedex.js
    Just adds the changes to the regi's. Nothing too big.

--- mods/bkrandommod/pokedex.js
+++ mods/bkrandommod/pokedex.js
@@ -1,1 +1,4 @@
 //This is how one makes comments w/o screwing up the code btw
+regirock:{num:377,species:"Regirock",types:["Rock"],gender:"N",baseStats:{hp:80,atk:100,def:200,spa:50,spd:100,spe:50},abilities:{0:"Clear Body","Sand Stream"},heightm:1.7,weightkg:230,color:"Brown",eggGroups:["No Eggs"]},
+regice:{num:378,species:"Regice",types:["Ice"],gender:"N",baseStats:{hp:80,atk:50,def:100,spa:100,spd:200,spe:50},abilities:{0:"Clear Body","Snow Warning"},heightm:1.8,weightkg:175,color:"Blue",eggGroups:["No Eggs"]},
+registeel:{num:379,species:"Registeel",types:["Steel"],gender:"N",baseStats:{hp:80,atk:75,def:150,spa:75,spd:150,spe:50},abilities:{0:"Clear Body","Air lock"},heightm:1.9,weightkg:205,color:"Gray",eggGroups:["No Eggs"]},
